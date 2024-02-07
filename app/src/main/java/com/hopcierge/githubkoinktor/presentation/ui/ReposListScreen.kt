2e32c9e25e61a7f7b338a40b361692abcec7692d
AnelCC <anel.calvo.cervantes@gmail.com>
2024-02-06 21:49:17 -0500

    Add simple UI

--- app/src/main/java/com/hopcierge/githubkoinktor/presentation/ui/ReposListScreen.kt
+++ app/src/main/java/com/hopcierge/githubkoinktor/presentation/ui/ReposListScreen.kt
@@ -1,7 +1,19 @@
 package com.hopcierge.githubkoinktor.presentation.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ReposListScreen() {
+    Column (Modifier
+        .fillMaxSize()
+        .padding(32.dp)
+    ){
+        Text(text = "Repos...")
+    }
 }
